470c24bbbb06903fe483322d8072a30508db69f5
NelsonHereveri <nelson@hereveri.cl>
2018-04-23 00:41:50 -0300

    Estructurando SW

--- sw.js
+++ sw.js
@@ -1,31 +1,45 @@
+let CACHE_NAME = 'pwa_prueba';
+
+/* Cualquier archivo que consideres estático (CSS, imágenes, fuentes, JS, plantillas, etc) */
+let STATIC_FILES = [
+  '/',
+  '.',
+  'index.html',
+  'manifest.json',
+  'css/critical.min.css',
+  'css/non-critical.min.css',
+  'img/gauge.png',
+  'img/gauge@0,25x.png',
+  'img/gauge@0,5x.png'
+];
+
+/* Igual que el anterior, pero no demora la instalación */
+let BIG_STATIC_FILES = [];
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('pwa_prueba').then(cache => {
-      return cache.addAll([
-        '/',
-        '.',
-        'index.html',
-        'manifest.json',
-        'css/critical.min.css',
-        'css/non-critical.min.css',
-        'img/gauge.png',
-        'img/gauge@0,25x.png',
-        'img/gauge@0,5x.png'
-      ])
-      .then(() => self.skipWaiting());
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(BIG_STATIC_FILES);
+      return cache.addAll(STATIC_FILES)
+        .then(() => self.skipWaiting());
     })
   )
 });
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(e.request).then(response => {
+        let fetchPromise = fetch(e.request).then(networkResponse => {
+          cache.put(e.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      });
     })
   );
 });
 
+self.addEventListener('activate',  e => {
+  e.waitUntil(self.clients.claim());
+});
